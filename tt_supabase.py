e3584fe029ec468461967b6aed09477a0ba63e6f
vrsssrikanth-glitch <vrsssrikanth@gmail.com>
2026-09-20 01:47:44 +0530

    Fix duplicate entry in TWO_PERIOD_SUBS set

--- tt_supabase.py
+++ tt_supabase.py
@@ -43,7 +43,7 @@ DAY_MAP = {
     "SATURDAY": "Saturday",
 }
 
-TWO_PERIOD_SUBS = {"Makers Lab", "EWS", "AIT", "DTI", "NSS", "HWYS","Makers Lab(Maths&Physics)", "Makerslab(Maths&Chemistry)"}
+TWO_PERIOD_SUBS = {"Makers Lab", "EWS", "AIT", "DTI", "NSS", "HWYS","Makers Lab(Maths&Physics)", "Makers Lab(Maths&Chemistry)"}
 THREE_PERIOD_SUBS = {"BEEE(ECE) Lab", "BEEE(EEE) Lab", "CHE Lab", "PHY Lab", "CP Lab", "DLD Lab"}
 EXCLUDE_THEORY_ROOM = {"DTI", "Makers Lab", "EGP", "EWS"}
 
